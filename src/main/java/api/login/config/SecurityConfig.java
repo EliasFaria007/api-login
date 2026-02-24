5a162e31f0f8fbe17a6ce477463395b7e0993f43
EliasFaria007 <140462565+EliasFaria007@users.noreply.github.com>
2026-02-24 13:34:56 -0300

    atualização

--- src/main/java/api/login/config/SecurityConfig.java
+++ src/main/java/api/login/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package api.login.config;
 
 import api.login.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.webmvc.ui.SwaggerConfig;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
